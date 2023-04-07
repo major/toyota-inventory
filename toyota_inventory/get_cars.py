78c049d99de3464f0fbfe3ce1ac177ab3e46fdea
Major Hayden <major@mhtx.net>
2023-04-06 19:20:27 -0500

    re-enable remote data

--- toyota_inventory/get_cars.py
+++ toyota_inventory/get_cars.py
@@ -7,7 +7,7 @@ import pandas as pd
 import requests
 
 # Set this to True to query local data instead of Toyota's API.
-QUERY_LOCAL_DATA = True
+QUERY_LOCAL_DATA = False
 
 GRAPHQL_QUERY = """query {
   locateVehiclesByZip(
